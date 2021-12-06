97929a94502ae3a8e2120909bb12201cb6b3c38e
Gerald Lee <glee325@gatech.edu>
2021-12-05 22:06:18 -0800

    Get JSON, Deserialize, Verify

--- CodingChallenge/MainWindow.xaml.cs
+++ CodingChallenge/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using CodingChallenge.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Text.Json;
 
 namespace CodingChallenge
 {
@@ -23,6 +18,18 @@ namespace CodingChallenge
         public MainWindow()
         {
             InitializeComponent();
+
+            // Get the data
+            getData();
+        }
+
+        private async void getData()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(@"https://api.json-generator.com/templates/M-rnKhH0Z6uf/data?access_token=edfgbmrjms4yldm77epct3jgyo3q65wxjinddzmc");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            var format1 = JsonSerializer.Deserialize<List<List<Format1>>>(json);
         }
     }
 }
---
+++ CodingChallenge/Models/Format1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace CodingChallenge.Models
+{
+    public class Format1
+    {
+        public List<StoreInformation> StoreInformation { get; set; } = new List<StoreInformation>();
+        public List<Transaction> transactions { get; set; } = new List<Transaction>();
+
+        public Format1()
+        {
+
+        }
+    }
+
+    public class StoreInformation
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string TimeZone { get; set; }
+
+        public StoreInformation()
+        {
+
+        }
+    }
+
+    public class Transaction
+    {
+        public string receiptId { get; set; }
+        public string createdDateUTC { get; set; }
+        public int budtenderId { get; set; }
+        public int customerId { get; set; }
+        public string transactionType { get; set; }
+        public List<Item> items { get; set; } = new List<Item>();
+        public List<DiscountInfo> transactionDiscounts { get; set; } = new List<DiscountInfo>();
+
+        public Transaction()
+        {
+
+        }
+    }
+
+    public class Item
+    {
+        public int itemId { get; set; }
+        public string category { get; set; }
+        public double cogs { get; set; }
+        public int quantity { get; set; }
+        public double basePrice { get; set; }
+        public double pricePaid { get; set; }
+        public List<DiscountInfo> itemDiscount { get; set; }
+
+        public Item()
+        {
+
+        }
+    }
+
+    public class DiscountInfo
+    {
+        public int discountId { get; set; }
+        public string name { get; set; }
+        public double amount { get; set; }
+
+        public DiscountInfo()
+        {
+
+        }
+    }
+}
