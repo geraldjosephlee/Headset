692e698c619120832cbd2216ab8ab65f92d0bf36
Gerald Lee <glee325@gatech.edu>
2021-12-06 19:46:34 -0800

    Get DailyTotals, Totals, Serialize, Save to File

--- CodingChallenge/MainWindow.xaml.cs
+++ CodingChallenge/MainWindow.xaml.cs
@@ -4,6 +4,9 @@ using System.Net.Http;
 using System.Windows;
 using System.Text.Json;
 using System.Linq;
+using System;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace CodingChallenge
 {
@@ -16,93 +19,144 @@ namespace CodingChallenge
         {
             InitializeComponent();
 
-            // Get the data
-            getData();
+            // Main method
+            main();
         }
 
-        private async void getData()
+        private async void main()
         {
+            // Get the data
+            var uri = @"https://api.json-generator.com/templates/M-rnKhH0Z6uf/data?access_token=edfgbmrjms4yldm77epct3jgyo3q65wxjinddzmc";
+            var format1 = await getData(uri);
+
+            // Parse the data
+            var format1JSON = parseData(format1);
+
+            // Print to file
+            File.WriteAllText(@"C:\Users\Public\challengeOutput.json", format1JSON);            
+        }
+
+        private async Task<List<List<Format1>>> getData(string uri)
+        {
+            // Get Format1 JSON data, ignore nulls
             var client = new HttpClient();
-            var response = await client.GetAsync
-                (@"https://api.json-generator.com/templates/M-rnKhH0Z6uf/data?access_token=edfgbmrjms4yldm77epct3jgyo3q65wxjinddzmc");
+            var response = await client.GetAsync(uri);
             response.EnsureSuccessStatusCode();
             var json = await response.Content.ReadAsStringAsync();
-            var format1 = JsonSerializer.Deserialize<List<List<Format1>>>(json, new JsonSerializerOptions() 
-                { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull});
+            var format1 = JsonSerializer.Deserialize<List<List<Format1>>>(json, new JsonSerializerOptions()
+            { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
+
+            return format1;
+        }
+
+        private string parseData(List<List<Format1>> format1)
+        {
+            // Initialize output
+            var format1JSON = new Format1Json();
+
+            // Initialize total metrics
+            var totalRevenue = 0.0;
+            var totalDiscount = 0.0;
+            var totalProfit = 0.0;
+            var totalTransactions = 0;
+            var totalItems = 0;
+            var totalSales = 0;
+            var totalReturns = 0;
+            var totalVoids = 0;
 
             // Get transactions
             var transactions = format1[1][0].transactions;
 
-            // Get list of all days
-            var days = transactions.Select(s => s.createdDateUTC).Distinct().ToList();
+            // Get list of days 
+            var days = transactions?.Select(t => t.createdDateUTC.Split('T')[0]).Distinct().ToList();
 
             // For each day...
             foreach (var day in days)
             {
                 // Get transactions
-                var dayTransactions = transactions.Where(dt => dt.createdDateUTC == day);
+                var dayTransactions = transactions.Where(dt => dt.createdDateUTC.Split('T')[0] == day);
 
                 // Get transaction types
                 var dayTransactionTypes = dayTransactions.Select(dtt => dtt.transactionType);
 
                 // Number of sales
-                var saleCount = dayTransactionTypes.Where(dtt => dtt == @"Sale").Count();
+                var dailySaleCount = dayTransactionTypes.Where(dtt => dtt == @"Sale").Count();
 
                 // Number of returns
-                var returnCount = dayTransactionTypes.Where(dtt => dtt == @"Return").Count();
+                var dailyReturnCount = dayTransactionTypes.Where(dtt => dtt == @"Return").Count();
 
                 // Number of voided transactions
-                var voidCount = dayTransactionTypes.Where(dtt => dtt == @"Void").Count();
+                var dailyVoidCount = dayTransactionTypes.Where(dtt => dtt == @"Void").Count();
 
-                // Number of items sold
-                var itemsSold = 0;
+                // Tally daily metics in one loop
+                var dailyItemsSold = 0;
+                var dailyNetRevenue = 0.0;
+                var dailyTotalDiscounts = 0.0;
+                var dailyProfit = 0.0;
+                var dailyNetItemCount = 0;
                 foreach (var dayTransaction in dayTransactions)
                 {
-                    if (dayTransaction.items.Count > 0)
-                    {                        
-                        if (dayTransaction.transactionType == "Sale")
-                        {
-                            itemsSold += dayTransaction.items.Count;
-                        }
-                        else if (dayTransaction.transactionType == "Return")
-                        {
-                            itemsSold -= dayTransaction.items.Count;
-                        }
+                    // Items sold
+                    if (dayTransaction.transactionType == "Sale")
+                    {
+                        dailyItemsSold += dayTransaction.items.Count;
                     }
-                }
+                    else if (dayTransaction.transactionType == "Return")
+                    {
+                        dailyItemsSold -= dayTransaction.items.Count;
+                    }
 
-                // Net Revenue (Need to review how to cast int? to double?)
-                double? netRevenue = 0.0;
-                foreach (var dayTransaction in dayTransactions)
-                {
+                    // Discounts (Transaction)
+                    dailyTotalDiscounts += dayTransaction.transactionDiscounts.Where(td => td.amount.HasValue).ToList().Sum(td => td.amount.Value);
+
+                    // Item Metrics
                     foreach (var item in dayTransaction.items)
                     {
-                        var revenue = (double?)item.quantity * item.pricePaid;
-                        netRevenue += revenue;
+                        // Discounts (Item)
+                        dailyTotalDiscounts += item.itemDiscount.Where(id => id.amount.HasValue).ToList().Sum(id => id.amount.Value);
+
+                        // Check for nulls
+                        if (item.quantity.HasValue && item.pricePaid.HasValue)
+                        {
+                            // Revenue
+                            dailyNetRevenue += item.quantity.Value * item.pricePaid.Value;
+
+                            // Check for null
+                            if (item.cogs.HasValue)
+                            {
+                                // Profit
+                                dailyProfit += item.quantity.Value * (item.pricePaid.Value - item.cogs.Value);
+                            }
+                        }
+
+                        // Item count
+                        dailyNetItemCount++;
                     }
                 }
 
-                // Total of Discounts applied
-                double? totalDiscounts = 0.0;
-                foreach (var dayTransaction in dayTransactions)
-                {
-                    // Sum transaction discounts list
-                    totalDiscounts += dayTransaction.transactionDiscounts.Sum(td => td.amount);
+                // Add to daily collection
+                format1JSON.Daily.Add(
+                    new Daily(day, dailySaleCount, dailyReturnCount, 
+                            dailyVoidCount, dailyNetRevenue, dailyTotalDiscounts, 
+                            dailyProfit, dailyNetItemCount));
 
-                    // Sum item discounts
-                    foreach (var item in dayTransaction.items)
-                    {
-                        totalDiscounts += item.itemDiscount.Sum(id => id.amount);
-                    }
-                }
-
-                // Profit for each day (Sale total - cogs)
-                double? profit = 0.0;
-                foreach (var dayTransaction in dayTransactions)
-                {
-                    dayTransaction.items.ForEach(i => profit += i.quantity * (i.pricePaid - i.cogs));
-                }
+                // Tally totals
+                totalRevenue += dailyNetRevenue;
+                totalDiscount += dailyTotalDiscounts;
+                totalProfit += dailyProfit;
+                totalTransactions += dayTransactions.Count();
+                totalItems += dailyNetItemCount;
+                totalSales += dailySaleCount;
+                totalReturns += dailyReturnCount;
+                totalVoids += dailyVoidCount;                
             }
+
+            // Append totals
+            format1JSON.Totals = new Totals(totalRevenue, totalDiscount, totalProfit, 
+                totalTransactions, totalItems, totalSales, totalReturns, totalVoids);
+
+            // Serialize to JSON
+            return JsonSerializer.Serialize(format1JSON, new JsonSerializerOptions() { WriteIndented = true});
         }
     }
 }
--- CodingChallenge/Models/Format1JSON.cs
+++ CodingChallenge/Models/Format1JSON.cs
@@ -4,47 +4,74 @@ namespace CodingChallenge.Models
 {
     public class Format1Json
     {
-        public Totals? Totals { get; set; }
-        public List<Daily>? Daily { get; set; }
+        public Totals Totals { get; set; } = new Totals();
+        public List<Daily> Daily { get; set; } = new List<Daily>();
 
         public Format1Json()
         {
 
         }
-
     }
 
     public class Daily
     {
-        public string Day { get; set; }
-        public int? Sales { get; set; }
-        public int? Returns { get; set; }
-        public int? Voids { get; set; }
-        public double? Revenue { get; set; }
-        public double? Discounts { get; set; }
-        public double? Profit { get; set; }
-        public int? NetItemCount { get; set; }
+        public string Day { get; set; } = string.Empty;
+        public int Sales { get; set; }
+        public int Returns { get; set; }
+        public int Voids { get; set; }
+        public double Revenue { get; set; }
+        public double Discounts { get; set; }
+        public double Profit { get; set; }
+        public int NetItemCount { get; set; }
 
         public Daily()
         {
 
         }
+
+        public Daily(string day, int sales, int returns, 
+            int voids, double revenue, double discounts, 
+            double profit, int netItemCount)
+        {
+            Day = day;
+            Sales = sales;
+            Returns = returns;
+            Voids = voids;
+            Revenue = revenue;
+            Discounts = discounts;
+            Profit = profit;
+            NetItemCount = netItemCount;
+        }
     }
 
     public class Totals
     {
-        public double? RevenueTotal { get; set; }
-        public double? DiscountTotal { get; set; }
-        public double? Profit { get; set; }
-        public int? TransCount { get; set; }
-        public int? ItemCount { get; set; }
-        public int? TotalSales { get; set; }
-        public int? TotalReturns { get; set; }
-        public int? TotalVoids { get; set; }
+        public double RevenueTotal { get; set; }
+        public double DiscountTotal { get; set; }
+        public double Profit { get; set; }
+        public int TransCount { get; set; }
+        public int ItemCount { get; set; }
+        public int TotalSales { get; set; }
+        public int TotalReturns { get; set; }
+        public int TotalVoids { get; set; }
 
         public Totals()
         {
 
+        }
+
+        public Totals(double revenueTotal, double discountTotal, 
+            double profit, int transCount, int itemCount, 
+            int totalSales, int totalReturns, int totalVoids)
+        {
+            RevenueTotal = revenueTotal;
+            DiscountTotal = discountTotal;
+            Profit = profit;
+            TransCount = transCount;
+            ItemCount = itemCount;
+            TotalSales = totalSales;
+            TotalReturns = totalReturns;
+            TotalVoids = totalVoids;
         }
     }
 }
